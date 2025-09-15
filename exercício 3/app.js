266f2d03c8823a816c0846ec8fbf6b941292887f
unknown <raissa.bez08@outlook.com>
2025-09-15 13:47:26 -0300

    commit final

--- exercício 3/app.js
+++ exercício 3/app.js
@@ -6,9 +6,18 @@ app.use(express.json());
 
 app.post ('/soma', (req, res) =>{
     try {
-        const {notas, nome} = req.body; // constante criada e atribuída para requisição do corpo da página em JSON
-        console.log(notas)
+        const {soma} = req.body.soma; // constante criada e atribuída para requisição do corpo da página em JSON
+        console.log(soma)
 
+        if (req.body.soma){
+            return res.status(400).send("Formato inválido");
+        }
+
+        if (soma) { // verificação de números, se realmente números foram digitados
+            return res.status(400).send("Apenas números devem ser digitados!");
+        }
+
+        res.status(201).json({message: `A soma dos dados é: ${dados.resultado} `});
     } catch (error) {
         console.log(error);
         res.status(500).json(error); // servidor não conectado
