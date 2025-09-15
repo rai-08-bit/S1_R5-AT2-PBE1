823191a51307116ad6e0983a5c030793e73a9c6f
unknown <raissa.bez08@outlook.com>
2025-09-15 13:37:46 -0300

    commitTwo

--- exercício 1/app.js
+++ exercício 1/app.js
@@ -7,37 +7,34 @@ app.use(express.json());
 
 app.post('/usuarios/novo', (req, res) => {
     try {
-        const { dadosNovo } = req.body; // constante criada e atribuída para requisição do corpo da página em JSON
-        console.log(dadosNovo);
+        const { user, senha, email } = req.body.dadosNovo; // constante criada e atribuída para requisição do corpo da página em JSON
+        console.log(user, senha, email);
 
-        if (fs.existsSync('./usuarios.json')) {
-            const usuarios = fs.readFileSync("./usuarios.json", "utf8");
-            const usuariosJson = JSON.parse(usuarios);
-        } else {
-            fs.writeFileSync('./usuarios.json', '[]');
-        }
 
-        if (dadosNovo.user.length < 3) {
+        if (user.length < 3) {
             return res.status(401).json("O nome de usuário deve ter no mínimo 3 caracteres. Digite novamente!");
         }
 
-        if (dadosNovo.senha.length < 4) {
+        if (senha.length < 4) {
             res.status(401).json("Sua senha deve ter no mínimo 4 caracteres. Digite novamente!");
         }
 
-        if (!dadosNovo.email.includes("@")) {
+        if (!email.includes("@")) {
             res.status(401).json("Seu email deve conter @, por favor, digite novamente!");
         }
-
-        let novoUsuario = {
-            user: dadosNovo.user,
-            senha: dadosNovo.senha,
-            email: dadosNovo.email
+        
+        let novoUsuario = { user, senha, email };
+        
+        let usuariosJson = [];
+        if (fs.existsSync('./usuarios.json')) {
+            usuariosJson = JSON.parse(fs.readFileSync("./usuarios.json", "utf8"));
+            usuariosJson.push(novoUsuario);
+            fs.writeFileSync('./usuarios.json', JSON.stringify(usuariosJson), 'utf8');
+        } else {
+            usuariosJson.push(novoUsuario);
+            fs.writeFileSync('./usuarios.json', JSON.stringify(usuariosJson), 'utf8');
         }
-
-        usuariosJson.push(novoUsuario);
-
-        fs.writeFileSync('./usuarios.json', JSON.stringify(usuariosJson));
+        res.status(201).json("Usuário criado com sucesso!");
         
 
     } catch (error) {
